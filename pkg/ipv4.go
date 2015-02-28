00cbf7c7b7c7892954330194b6d32111b1cf6f6c
Yasha Bubnov <girokompass@gmail.com>
2015-03-01 00:33:37 +0300

    LGPL license

--- pkg/ipv4.go
+++ pkg/ipv4.go
@@ -1,4 +1,28 @@
 package pkg
 
+import (
+	"net"
+)
+
+type IPv4HeaderFlags int
+
+const (
+	DontFragment IPv4HeaderFlags = 1 + iota
+	MoreFragment
+)
+
 type IPv4 struct {
+	Version  int
+	Len      int
+	TOS      int
+	TotalLen int
+	ID       int
+	Flags    IPv4HeaderFlags
+	FragOff  int
+	TTL      int
+	Protocol int
+	Checksum int
+	Src      net.IP
+	Dst      net.IP
+	Options  []byte
 }
